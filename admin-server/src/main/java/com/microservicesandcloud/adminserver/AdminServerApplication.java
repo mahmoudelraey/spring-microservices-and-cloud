87ac233ddbb658a9c208f01e206a22ec55ef5e86
DESKTOP-P02I6O6\Mahmoud <mahmoudelraey123@gmail.com>
2022-10-03 09:52:52 +0200

    enable admin server configuration

--- admin-server/src/main/java/com/microservicesandcloud/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/microservicesandcloud/adminserver/AdminServerApplication.java
@@ -1,10 +1,11 @@
 package com.microservicesandcloud.adminserver;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Ena
+@EnableAdminServer
 public class AdminServerApplication {
 
 	public static void main(String[] args) {
